1c620f74e1338500f703d666fc7a3d8638c7130c
Drazyn <gabriellucena360@outlook.com>
2017-06-17 20:54:08 -0300

    Add files via upload

---
+++ otclientrc.lua
@@ -0,0 +1,5 @@
++t8G;`5hG: 6X!?/)J(tgUGc&lN$`>nO*l:X!22-ZC#5bVW&5"KZ!.:uB>l6Qd7JeW5;sPg:l_)#6!J1yHt 4'9'cAe;(JZ %!d!U
+U[s:.<Z gAGs$lK&%s;vx
+U!YJBp%F^99^F%pBJY!U
+U!YJBp%F^99^F%pBJY!U
+U!Y
